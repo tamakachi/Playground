a0031312712869997317aca9faf46c6c5e5209b3
Kent <kenttechnical@gmail.com>
2022-09-22 19:50:33 +0200

    first commit

---
+++ Playground/src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+  <Counter></Counter>
+</main>
+
+<style>
+
+</style>
---
+++ Playground/src/lib/Counter.svelte
@@ -0,0 +1,21 @@
+<script>
+  let random = [0]
+
+  const addRandom = () =>{
+    
+    console.log(random)
+    random = [...random, Math.random()]
+    // The correct way to push an array into another one, this way refreshes the value bind,
+    // When using random.push(Math.random()) the information does not refresh
+  }
+
+  let count = 0
+  const increment = () => {
+    count += 1
+  }
+</script>
+
+<button on:click={increment}>
+  count is {count}
+</button>
+<button on:click={addRandom}>{random}</button>
