b3525183a876b95e4f23655bf768556428c39e4d
tamakachi <kenttechnical@gmail.com>
2022-10-03 17:33:05 +0200

    Print even numbers 0-10

--- Playground/src/App.svelte
+++ Playground/src/App.svelte
@@ -4,7 +4,14 @@
 
 <script>
   import Counter from './lib/Counter.svelte'
-</script>
+  const numbers = [0,1,2,3,4,5,6,7,8,9,10]
+  const filteredNumbers = numbers.filter((number)=>{
+    if (number !== 0){  
+    return number % 2 === 0 
+    }
+  })
+  console.log(filteredNumbers)
+  </script>
 
 <main>
   <div>
