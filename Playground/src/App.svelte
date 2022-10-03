905b0db3efd4b76c5f7dc365715c9c218c4d706b
tamakachi <kenttechnical@gmail.com>
2022-10-03 18:04:32 +0200

    Times table completed

--- Playground/src/App.svelte
+++ Playground/src/App.svelte
@@ -4,13 +4,22 @@
 
 <script>
   import Counter from './lib/Counter.svelte'
-  const numbers = [0,1,2,3,4,5,6,7,8,9,10]
-  const filteredNumbers = numbers.filter((number)=>{
-    if (number !== 0){  
-    return number % 2 === 0 
+  
+  let currentFirstMultiple = 1
+  let secondMultiple = 1
+  while(currentFirstMultiple<=13 && secondMultiple<=13)
+  {
+
+    if(secondMultiple>12){
+      currentFirstMultiple++
+      secondMultiple = 1
     }
-  })
-  console.log(filteredNumbers)
+
+    console.log(`${currentFirstMultiple} * ${secondMultiple} = ${currentFirstMultiple*secondMultiple}`)
+    secondMultiple++
+    
+  }
+
   </script>
 
 <main>
