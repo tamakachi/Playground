34af2c0a06fb377f8fcd80f3a3476a56e749c723
tamakachi <kenttechnical@gmail.com>
2022-10-18 16:39:57 +0200

    reactive declarations and statements

--- Playground/src/App.svelte
+++ Playground/src/App.svelte
@@ -4,37 +4,58 @@
 
   <script>
 
-    
-    let imageUrl = 'https://svelte.dev/svelte-logo-horizontal.svg'
-  
+    let count =1
 
-    let myArray = [0,1]
-    let myVariable = 1
+    // Reactive declaration 
+    // Updates the variable whenever the dependency changes (everything right of equals)
+
+    $: double = count * 2
+    $: console.log(count)
+    $: console.log('reliable double: ', double)
+
+    //Reactive statements
+    // Fires the function every time the dependencies change values
+
+    $: add(count,double)
+
+    function add(count,double){
+      console.log("Added = ", count + double)
+    }
 
     function increment(){
-      myArray.push(1)
-      myArray = myArray
+      count++
 
     }
 
-    function updateVar(){
-      myVariable = 2
+    function decrement(){
+      count--
     }
 
-    import Component from "./lib/Component.svelte"
-  </script>
+    function doSomething(){
+      console.log(double)
+    }
+
+    </script>
 
 <main>
-  <h1>{myArray}</h1>
-  <h1>{myVariable}</h1>
-  <button on:click={increment}>Push into Array</button>
-  <button on:click={updateVar}>Update Var</button>
+  <div class="text-center pt-10 font-bold">
+    <button 
+      class="border border-solid border-black px-[1em] py-[0.6em] rounded-full hover:bg-slate-900 hover:text-white" 
+      on:click={decrement}>-
+    </button>
+    {count} * 2 = {double}
+    <button 
+      class="border border-solid border-black px-[1em] py-[0.6em] rounded-full hover:bg-slate-900 hover:text-white" 
+      on:click={increment}>+
+    </button>
+    <button 
+    class="border border-solid border-black px-[1em] py-[0.6em] rounded-full hover:bg-slate-900 hover:text-white" 
+    on:click={doSomething}>Log
+  </button>
+  </div>
 </main>
 
 <style>
 
-  h1{ 
-    color: rebeccapurple;
-  }
 
 </style>
