86e154c39607e6465a19bf5d3368d6c1f4175252
tamakachi <kenttechnical@gmail.com>
2022-10-19 13:30:54 +0200

    Practice code

--- Playground/src/App.svelte
+++ Playground/src/App.svelte
@@ -2,57 +2,22 @@
 	<script src="https://cdn.tailwindcss.com"></script>
 </svelte:head>
 
-  <script>
-
-    let count =1
-
-    // Reactive declaration 
-    // Updates the variable whenever the dependency changes (everything right of equals)
-
-    $: double = count * 2
-    $: console.log(count)
-    $: console.log('reliable double: ', double)
-
-    //Reactive statements
-    // Fires the function every time the dependencies change values
-
-    $: add(count,double)
-
-    function add(count,double){
-      console.log("Added = ", count + double)
-    }
-
-    function increment(){
-      count++
-
-    }
-
-    function decrement(){
-      count--
-    }
-
-    function doSomething(){
-      console.log(double)
-    }
-
-    </script>
+<script>
+ let people = ['Kent','Ben','Max','Ashleigh']
+ let count = 0
+ $:double = count * 2
+ $: console.log(`${count} is ${double} / 2`)
+</script>
 
 <main>
-  <div class="text-center pt-10 font-bold">
-    <button 
-      class="border border-solid border-black px-[1em] py-[0.6em] rounded-full hover:bg-slate-900 hover:text-white" 
-      on:click={decrement}>-
-    </button>
-    {count} * 2 = {double}
-    <button 
-      class="border border-solid border-black px-[1em] py-[0.6em] rounded-full hover:bg-slate-900 hover:text-white" 
-      on:click={increment}>+
-    </button>
-    <button 
-    class="border border-solid border-black px-[1em] py-[0.6em] rounded-full hover:bg-slate-900 hover:text-white" 
-    on:click={doSomething}>Log
-  </button>
-  </div>
+  <button on:click={()=>{count--}}>-</button>
+  {count} * 2 = {double} 
+  <button on:click={()=>{count++}}>+</button>
+  <button on:click={()=>{count=0}}>Reset</button>
+  <form action="">
+  <button on:click|preventDefault={()=>{people.push('Legend'), people = people}}>Push into Array</button>
+  </form>
+  <p>{people}</p>
 </main>
 
 <style>
--- Playground/src/lib/Component.svelte
+++
@@ -1,5 +0,0 @@
-<h1>Another Component</h1>
-
-<p>Bye World</p>
-
-<div> <a href="https://www.twitter.com">Link to my twitter</a></div>
