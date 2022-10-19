089e4e48054386488ad1b2f3583a5e4d36d8d4a0
tamakachi <kenttechnical@gmail.com>
2022-10-19 15:11:48 +0200

    Learning

--- Playground/src/App.svelte
+++ Playground/src/App.svelte
@@ -3,21 +3,32 @@
 </svelte:head>
 
 <script>
+
+  import { tick } from 'svelte'
+  import Component from './lib/Component.svelte'
+
  let people = ['Kent','Ben','Max','Ashleigh']
+ let details = {username:'Kent',email:'kent@gmail.com',age:30}
  let count = 0
  $:double = count * 2
  $: console.log(`${count} is ${double} / 2`)
+
+ let greetUser = (event) =>{
+    console.log('Greetings ' + event.detail)
+ }
+
 </script>
 
 <main>
   <button on:click={()=>{count--}}>-</button>
   {count} * 2 = {double} 
-  <button on:click={()=>{count++ ,console.log(double)}}>+</button>
+  <button on:click={()=>{count++ ,tick().then(()=>{console.log(double)})}}>+</button>
   <button on:click={()=>{count=0}}>Reset</button>
   <form action="">
     <button on:click|preventDefault={()=>{people.push('Legend'), people = people}}>Push into Array</button>
   </form>
   <p>{people}</p>
+  <Component {...details} on:greet={greetUser}></Component>
 </main>
 
 <style>
---
+++ Playground/src/lib/Component.svelte
@@ -0,0 +1,21 @@
+<script>
+    import {createEventDispatcher} from 'svelte'
+    export let username = 'user'
+    export let age
+
+    let dispatch = createEventDispatcher()
+
+    function greetUser(){
+        dispatch('greet', username)
+    }
+
+</script>
+
+<main>
+    <h4>Greetings {username}, you are {age} years old!</h4>
+    <button on:click={greetUser}>Greet user</button>
+</main>
+
+<style>
+
+</style>
