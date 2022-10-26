f55aae0c75e3eaacf427645fa4055a3e972589a1
tamakachi <kenttechnical@gmail.com>
2022-10-26 17:14:33 +0200

    learning

--- Playground/src/App.svelte
+++ Playground/src/App.svelte
@@ -3,35 +3,49 @@
 </svelte:head>
 
 <script>
+	let scoops = 1;
+	let flavours = ['Mint choc chip'];
 
-  import { tick } from 'svelte'
-  import Component from './lib/Component.svelte'
-
- let people = ['Kent','Ben','Max','Ashleigh']
- let details = {username:'Kent',email:'kent@gmail.com',age:30}
- let count = 0
- $:double = count * 2
- $: console.log(`${count} is ${double} / 2`)
-
- let greetUser = (event) =>{
-    console.log('Greetings ' + event.detail)
- }
-
+	function join(flavours) {
+		if (flavours.length === 1) return flavours[0];
+		return `${flavours.slice(0, -1).join(', ')} and ${flavours[flavours.length - 1]}`;
+	}
 </script>
 
-<main>
-  <button on:click={()=>{count--}}>-</button>
-  {count} * 2 = {double} 
-  <button on:click={()=>{count++ ,tick().then(()=>{console.log(double)})}}>+</button>
-  <button on:click={()=>{count=0}}>Reset</button>
-  <form action="">
-    <button on:click|preventDefault={()=>{people.push('Legend'), people = people}}>Push into Array</button>
-  </form>
-  <p>{people}</p>
-  <Component {...details} on:greet={greetUser}></Component>
-</main>
+<h2>Size</h2>
 
-<style>
+<label>
+	<input type=radio bind:group={scoops} name="scoops" value={1}>
+	One scoop
+</label>
 
+<label>
+	<input type=radio bind:group={scoops} name="scoops" value={2}>
+	Two scoops
+</label>
 
-</style>
+<label>
+	<input type=radio bind:group={scoops} name="scoops" value={3}>
+	Three scoops
+</label>
+
+<p>scoops: {scoops}</p>
+
+<h2>Flavours</h2>
+
+<label>
+	<input type=checkbox bind:group={flavours} name="flavours" value="Cookies and cream">
+	Cookies and cream
+</label>
+
+<label>
+	<input type=checkbox bind:group={flavours} name="flavours" value="Mint choc chip">
+	Mint choc chip
+</label>
+
+<label>
+	<input type=checkbox bind:group={flavours} name="flavours" value="Raspberry ripple">
+	Raspberry ripple
+</label>
+
+<p>{flavours}</p>
